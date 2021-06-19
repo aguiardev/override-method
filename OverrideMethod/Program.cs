3e8d86c91831b4a7fd8391cd3efe5668fea9c82e
Tiago Aguiar <tiago.aguiar1@hotmail.com>
2021-06-19 16:10:32 -0300

    example override method

---
+++ OverrideMethod/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OverrideMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
